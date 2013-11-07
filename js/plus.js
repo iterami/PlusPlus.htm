bff94199aac905f29c3ea7802f9da44044679772
Jan Horava <czechinthemail@gmail.com>
2013-11-06 23:09:39 -0800

    added localStorage

--- js/plus.js
+++ js/plus.js
@@ -1,14 +1,31 @@
+function get(i){
+    return document.getElementById(i);
+}
+
 function plus(){
-    document.getElementById('number').innerHTML = parseInt(document.getElementById('number').innerHTML) + 1;
+    get('number').innerHTML = parseInt(get('number').innerHTML) + 1;
 }
 
 function reset(){
     if(confirm('Reset?')){
-        document.getElementById('number').innerHTML = 0;
+        localStorage.removeItem('plus-0');
+        get('number').innerHTML = 0;
     }
 }
 
 var unlocked = 1;
+get('number').innerHTML = localStorage.getItem('plus-0') === null
+    ? 0
+    : localStorage.getItem('plus-0');
+
+window.onbeforeunload = function(e){
+    if(parseInt(get('number').innerHTML) > 0){
+        localStorage.setItem(
+            'plus-0',
+            get('number').innerHTML
+        );
+    }
+}
 
 window.onkeydown = function(e){
     if(unlocked){
